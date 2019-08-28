56e13a5c4ae2b0db16236b6d6653015681872a19
Harvey-46904 <harveympv@hotmail.com>
2019-08-28 10:21:03 -0500

    v4

--- ejemplos.py
+++ ejemplos.py
@@ -1,1 +1,13 @@
-print ("hola mundo")
+
+#variables
+#int , str, bool, float 
+def persona(nombre,apellido):
+    print("hola",nombre, "su apellido es :",apellido)
+
+def suma(a,b):
+    resultado=a+b
+    print("la suma es : ",resultado)
+
+a=int(input("Digite a: "))
+b=int(input("Digite b: "))
+suma(a,b)
